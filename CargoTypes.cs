9c385e456ec1330ffcd3a3dad819fc74bb8e8022
pinteresz <pinteresz@gmail.com>
2023-07-15 16:16:08 +0200

    feat: add enum CargoTypes

---
+++ CargoTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecool_starfleet_control
+{
+    public enum CargoTypes
+    {
+        Food,
+        Medicine,
+        Weapons
+    }
+}
