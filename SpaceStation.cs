62d80bb41811cf091a2bfc8c5e9b2ca914af9510
pinteresz <pinteresz@gmail.com>
2023-07-15 16:52:19 +0200

    feat: add method DockShip and UndockShip in class SpaceStation

--- SpaceStation.cs
+++ SpaceStation.cs
@@ -22,6 +22,25 @@ namespace codecool_starfleet_control
             CraftType = CraftTypes.SpaceStation;
             IsInSpace = false;
         }
+        public void DockShip(MobileSpaceCraft spacecraft)
+        {
+            if (dockedCrafts.Count < maxNumberToDock)
+            {
+                dockedCrafts.Add(spacecraft);
+                spacecraft.IsInSpace = false;
+                Console.WriteLine($"Ship {spacecraft.Name} docked at Space Station {RegistrationCode}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry but the spaceship {spacecraft.Name} can not dock here, there are no available docking slots.");
+            }
+        }
+
+        public void UndockShip(SpaceCraft spacecraft)
+        {
+            dockedCrafts.Remove(spacecraft);
+            spacecraft.IsInSpace = true;
+        }
     }
 
 }
