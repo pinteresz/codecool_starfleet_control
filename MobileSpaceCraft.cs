a04547f1b94272fee7ef3cccd7ad7d76518de0f1
pinteresz <pinteresz@gmail.com>
2023-07-15 16:07:45 +0200

    feat: add abstract class MobileSpaceCraft

---
+++ MobileSpaceCraft.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecool_starfleet_control
+{
+    public abstract class MobileSpaceCraft : SpaceCraft
+    {
+        public int MaxSpeed { get; protected set; }
+        public MobileSpaceCraft(string name, int builtOfYear) : base(name, builtOfYear)
+        {
+            RegistrationCode = Guid.NewGuid();
+            Name = name;
+            YearOfBuilt = builtOfYear;
+            IsInSpace = true;
+
+            if (YearOfBuilt < 2150)
+            {
+                MaxSpeed = 150 - 30;
+            }
+            if (YearOfBuilt > 2200)
+            {
+                MaxSpeed = 150 + 15;
+            }
+            else
+            {
+                MaxSpeed = 150;
+            }
+        }
+
+        public abstract void DockAtSpaceStation(SpaceStation spaceStation);
+    }
+}
