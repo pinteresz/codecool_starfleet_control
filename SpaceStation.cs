87e1369016a4f56c9bf923cb6b3ff78734c344af
pinteresz <pinteresz@gmail.com>
2023-07-15 16:40:35 +0200

    feat: add class SpaceStation

---
+++ SpaceStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace codecool_starfleet_control
+{
+    public class SpaceStation : SpaceCraft
+    {
+        private List<SpaceCraft> dockedCrafts { get; set; }
+
+        private int maxNumberToDock = 2;
+
+        public SpaceStation(string name, int yearOfBuilt) : base(name, yearOfBuilt)
+        {
+            Name = name;
+            YearOfBuilt = yearOfBuilt;
+            RegistrationCode = Guid.NewGuid();
+            dockedCrafts = new List<SpaceCraft>();
+            CraftType = CraftTypes.SpaceStation;
+            IsInSpace = false;
+        }
+    }
+
+}
